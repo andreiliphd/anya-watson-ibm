093e9cf0f17f127939e8dc099946d1c33382c539
Andrei Li <andreiliphd@gmail.com>
2020-11-16 11:24:45 +0300

    Turn on logging.

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def main():
     q = request.form.get('q')
+    logging.log(0, q)
     appid = request.form.get('appid')
+    logging.log(0, appid)
     response = requests.get('http://api.openweathermap.org/data/2.5/weather',
                  params = [('q', q), ('appid', appid)])
     return response.json()
