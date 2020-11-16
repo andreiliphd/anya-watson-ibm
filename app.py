c099e0d2380ca34f93e47816b70fb481cb5afa57
Andrei Li <andreiliphd@gmail.com>
2020-11-16 10:31:10 +0300

    Implemented converter.

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, escape, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+@app.route('/', methods=['POST'])
+def main():
+    q = request.form.get('q')
+    appid = request.form.get('appid')
+    response = requests.get('api.openweathermap.org/data/2.5/weather',
+                 params = [('q', q), ('appid', appid)])
+    return response.json()
