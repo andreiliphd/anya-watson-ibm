a059de0cacc10b59e378fe5d7259c0d9525d98c6
Andrei Li <andreiliphd@gmail.com>
2020-11-16 11:33:29 +0300

    Turn on deeper logging.

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, escape, request
 import requests
 import logging
 
+logger = logging.getLogger('Anya')
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def main():
     q = request.form.get('q')
-    logging.log(0, q)
+    logger.info(q)
     appid = request.form.get('appid')
-    logging.log(0, appid)
+    logger.info(appid)
     response = requests.get('http://api.openweathermap.org/data/2.5/weather',
                  params = [('q', q), ('appid', appid)])
     return response.json()
