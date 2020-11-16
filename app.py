4b7dc74fb9428221b99a983de9ba727ebf62c2c5
Andrei Li <andreiliphd@gmail.com>
2020-11-16 11:47:02 +0300

    Logging config.

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ from flask import Flask, escape, request
 import requests
 import logging
 
-logger = logging.getLogger('Anya')
-
 app = Flask(__name__)
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
 
 @app.route('/', methods=['POST'])
 def main():
     q = request.form.get('q')
-    app.logger.info(q)
+    app.logger.warning(q)
     appid = request.form.get('appid')
-    app.logger.info(appid)
+    app.logger.warning(appid)
     response = requests.get('http://api.openweathermap.org/data/2.5/weather',
                  params = [('q', q), ('appid', appid)])
     return response.json()
